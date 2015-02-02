2ffb0f2b7d4598ebdef4b0a1cfa64e4782d8ac14
nighthawk702 <alexandru.ungurianu@gmail.com>
2015-02-02 12:18:56 +0200

    Pulled out the components and changed the base class

--- simulator/simulator.ts
+++ simulator/simulator.ts
@@ -1,3 +1,5 @@
+/// <reference path="../libs/jquery.d.ts" />
+
 module Simulator {
     export class Connection {
         value:boolean;
@@ -9,6 +11,7 @@ module Simulator {
         outputs:Array<Connection>;
         evaluate:() => void;
         name:string;
+        contDiv:JQuery;
 
         constructor(inputSize:number, outputSize:number) {
             this.inputs = new Array<Connection>();
@@ -62,17 +65,6 @@ module Simulator {
                 }
             }
         }
-
-    }
-
-    export class Printer extends Component {
-        constructor(id:string = "") {
-            super(1,0);
-            this.name = "Printer_" + id;
-            this.evaluate = function() {
-                console.log(this.inputs[0].value);
-            }
-        }
     }
 
     var connections = new Array<Connection>();
@@ -87,65 +79,4 @@ module Simulator {
     }
 }
 
-module Simulator.LogicalComponents {
-    export class True extends Component {
-        constructor(id:string = "") {
-            super(0,1);
-            this.name="True_"+id;
-            this.evaluate = function() {
-                if(this.outputs[0] != undefined) {
-                    this.outputs[0].value = true;
-                }
-            }
-        }
-    }
-
-    export class False extends Component {
-        constructor(id:string = "") {
-            super(0,1);
-            this.name="False_"+id;
-            this.evaluate = function() {
-                if(this.outputs[0] != undefined) {
-                    this.outputs[0].value = false;
-                }
-            }
-        }
-    }
-
-    export class Not extends Component {
-        constructor(id:string = "") {
-            super(1,1);
-            this.name = "Not_"+id;
-            this.evaluate = function() {
-                if(this.outputs[0] != undefined) {
-                    this.outputs[0].value = !this.inputs[0].value;
-                }
-            }
-        }
-    }
-
-    export class Or extends Component {
-        constructor(id:string = "") {
-            super(2,1);
-            this.name = "Or_"+id;
-            this.evaluate = function() {
-                if(this.outputs[0] != undefined) {
-                    this.outputs[0].value = this.inputs[0].value || this.inputs[1].value;
-                }
-            }
-        }
-    }
-
-    export class And extends Component {
-        constructor(id:string = "") {
-            super(2,1);
-            this.name = "And_"+id;
-            this.evaluate = function() {
-                if(this.outputs[0] != undefined) {
-                    this.outputs[0].value = this.inputs[0].value && this.inputs[1].value;
-                }
-            }
-        }
-    }
-}
 
