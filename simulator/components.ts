8e43e834a5fd8386a84d9bd5b4974cfe4678ec88
Alexandru Ungurianu <alexandru.ungurianu@gmail.com>
2015-05-18 18:25:41 +0300

    Translated js hot-fixes to typescript

--- simulator/components.ts
+++ simulator/components.ts
@@ -30,8 +30,9 @@ module Components {
             super(nInputs,nOutputs);
             this.contDiv = $("<div id=" + this.name + "></div>").addClass("component").addClass(compName);
             $("#screen").append(this.contDiv);
+            var spos = $("#screen").offset()
             Simulator.addComponent(this.name,this);
-            this.contDiv.offset({top:posy,left:posx});
+            this.contDiv.offset({ top: spos.top + posy, left: spos.left + posx });
             jsPlumb.draggable(this.contDiv, {containment:$("#screen")});
         }
     }
@@ -47,19 +48,20 @@ module Components {
             this.value = false;
 
             super(0,1,posx,posy,Switch.componentName);
-            this.contDiv.css("background-color","red");
 
-            this.contDiv.click({parent:this},function (event) {
-                if(event.data.parent.value) {
+            this.contDiv.append($("<img src=\"simulator/gates/switch_off.png\"></>"));
+
+            this.contDiv.click({ parent: this }, function (event) {
+                if (event.data.parent.value) {
                     event.data.parent.value = false;
-                    event.data.parent.contDiv.css("background-color","red");
+                    event.data.parent.contDiv.children("img").attr("src","simulator/gates/switch_off.png");
                 }
                 else {
                     event.data.parent.value = true;
-                    event.data.parent.contDiv.css("background-color","green");
+                    event.data.parent.contDiv.children("img").attr("src","simulator/gates/switch_on.png");
                 }
                 event.data.parent.update();
-            })
+            });
 
             jsPlumb.addEndpoint(this.contDiv,{anchor:"Right"},outputEndpoint).id = this.name+"-o0";
 
@@ -79,56 +81,57 @@ module Components {
 
             super(1,0,posx,posy,Light.componentName);
 
-            jsPlumb.addEndpoint(this.contDiv,{anchor:"Left"},inputEndpoint).id = this.name+"-i0";
+            this.contDiv.append($("<img src=\"simulator/gates/light_off.png\"></>"));
 
+            jsPlumb.addEndpoint(this.contDiv,{anchor:"Bottom"},inputEndpoint).id = this.name+"-i0";
 
             this.evaluate = function () {
-                if(this.inputValue[0]) {
-                    this.contDiv.css("background-color","green");
+                if (this.inputValue[0]) {
+                    this.contDiv.children("img").attr("src","simulator/gates/light_on.png");
                 }
                 else {
-                    this.contDiv.css("background-color","red");
+                    this.contDiv.children("img").attr("src","simulator/gates/light_off.png");
                 }
             }
         }
         removeInput(index:number):void {
             super.removeInput(index);
-            this.contDiv.css("background-color","blue");
+            this.contDiv.children("img").attr("src","simulator/gates/light_off.png");
         }
     }
 
-    export class True extends HTMLComponent {
-        static compCount:number = 0;
-        static componentName:string = "true";
-        constructor(posx:number,posy:number) {
-            this.name = True.componentName + True.compCount;
-            True.compCount+=1;
+    // export class True extends HTMLComponent {
+    //     static compCount:number = 0;
+    //     static componentName:string = "true";
+    //     constructor(posx:number,posy:number) {
+    //         this.name = True.componentName + True.compCount;
+    //         True.compCount+=1;
 
-            super(0,1,posx,posy,True.componentName);
+    //         super(0,1,posx,posy,True.componentName);
 
-            jsPlumb.addEndpoint(this.contDiv,{anchor:"Right"},outputEndpoint).id = this.name+"-o0";
+    //         jsPlumb.addEndpoint(this.contDiv,{anchor:"Right"},outputEndpoint).id = this.name+"-o0";
 
-            this.evaluate = function() {
-                this.outputValue[0] = true;
-            }
-        }
-    }
+    //         this.evaluate = function() {
+    //             this.outputValue[0] = true;
+    //         }
+    //     }
+    // }
 
-    export class False extends HTMLComponent {
-        static compCount:number = 0;
-        static componentName:string = "false";
-        constructor(posx:number,posy:number) {
-            this.name = False.componentName + False.compCount;
-            False.compCount+=1;
+    // export class False extends HTMLComponent {
+    //     static compCount:number = 0;
+    //     static componentName:string = "false";
+    //     constructor(posx:number,posy:number) {
+    //         this.name = False.componentName + False.compCount;
+    //         False.compCount+=1;
 
-            super(0,1,posx,posy,False.componentName);
+    //         super(0,1,posx,posy,False.componentName);
 
-            jsPlumb.addEndpoint(this.contDiv,{anchor:"Right"},outputEndpoint).id = this.name+"-o0";
-            this.evaluate = function() {
-                this.outputs[0].value = false;
-            }
-        }
-    }
+    //         jsPlumb.addEndpoint(this.contDiv,{anchor:"Right"},outputEndpoint).id = this.name+"-o0";
+    //         this.evaluate = function() {
+    //             this.outputs[0].value = false;
+    //         }
+    //     }
+    // }
 
     export class Not extends HTMLComponent {
         static compCount:number = 0;
@@ -208,7 +211,7 @@ module Components {
             }
         }
     }
-
+}
     // export class Splitter extends HTMLComponent {
     //     static compCount:number = 0;
     //     static componentName:string = "splitter";
@@ -233,7 +236,6 @@ module Components {
     //         }
     //     }
     // }
-}
 
 jsPlumb.ready(function() {
 
@@ -245,7 +247,7 @@ jsPlumb.ready(function() {
             var targetid = info.target.id;
             var sourceepid = +(info.sourceEndpoint.id.split("-")[1].substring(1));
             var targetepid = +(info.targetEndpoint.id.split("-")[1].substring(1));
-            console.log(sourceepid,targetepid);
+            console.log("Connection detached from ", sourceid,"(",sourceepid, ") to ",targetid,"(",targetepid,").");
             //Simulator.connect(Simulator.getComponent(targetid),targetepid,Simulator.getComponent(sourceid),sourceepid);
             Simulator.connect(Simulator.getComponent(sourceid),sourceepid,Simulator.getComponent(targetid),targetepid);
         });
@@ -270,5 +272,4 @@ jsPlumb.ready(function() {
             //Simulator.disconnect(Simulator.getComponent(targetid),targetepid,Simulator.getComponent(sourceid),sourceepid);
         });
     });
-
 });
